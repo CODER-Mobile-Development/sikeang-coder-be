90ca730d10c80356737e4cbd1bd507d5389a8d93
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-10-11 12:38:40 +0700

    add "update event data" endpoint

--- app/event/controller.js
+++ app/event/controller.js
@@ -15,5 +15,31 @@ module.exports = {
             message: "Gagal membuat data Event, silahkan coba beberapa saat lagi."
           })
         })
+  },
+  editEventData: (req, res) => {
+    const {eventName, startDate, endDate, description, photoUrl, eventType, eventDivision} = req.body;
+    const {id} = req.query
+
+    const updatedData = {}
+
+    if (eventName) updatedData.eventName = eventName
+    if (startDate) updatedData.startDate = startDate
+    if (endDate) updatedData.endDate = endDate
+    if (description) updatedData.description = description
+    if (photoUrl) updatedData.photoUrl = photoUrl
+    if (eventType) updatedData.eventType = eventType
+    if (eventDivision) updatedData.eventDivision = eventDivision
+
+    Event.findByIdAndUpdate(id, updatedData, {new: true})
+        .then(r => {
+          res.status(200).json({error: false, data: r})
+        })
+        .catch(e => {
+          console.log(e)
+          res.status(500).json({
+            error: true,
+            message: "Gagal mengubah data Event, silahkan coba beberapa saat lagi."
+          })
+        })
   }
 }
--- app/event/router.js
+++ app/event/router.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {addEventData} = require("./controller");
+const {addEventData, editEventData} = require("./controller");
 const {requireAdmin} = require("../../middleware/authentication");
 const router = express.Router()
 
 router.post('/', requireAdmin, addEventData)
+router.put('/', requireAdmin, editEventData)
 
 module.exports = router
