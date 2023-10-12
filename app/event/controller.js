e73a1afe36988f6c423d5e85ac78a9b2310e6d43
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-10-12 17:04:15 +0700

    add endpoint findEventData

--- app/event/controller.js
+++ app/event/controller.js
@@ -1,4 +1,5 @@
 const Event = require('../../model/Event')
+const moment = require("moment");
 
 module.exports = {
   addEventData: (req, res) => {
@@ -39,6 +40,65 @@ module.exports = {
           res.status(500).json({
             error: true,
             message: "Gagal mengubah data Event, silahkan coba beberapa saat lagi."
+          })
+        })
+  },
+  findEventData: async (req, res) => {
+    const {query, id, eventTime, eventType} = req.query
+    const {division, position} = req.userData
+
+    let dbQuery = {}
+
+    if (query === "id") dbQuery = {_id: id}
+    if (query === "eventStatus") {
+      const currentDateAndTime = new moment()
+      if (eventTime === "live") {
+        dbQuery = {
+          eventType: eventType, $and: [
+            {startDate: {$lte: currentDateAndTime}},
+            {endDate: {$gte: currentDateAndTime}}
+          ]
+        }
+      } else if (eventTime === "upcoming") {
+        dbQuery = {
+          eventType: eventType,
+          $and: [
+            {startDate: {$gt: currentDateAndTime}},
+            {endDate: {$gt: currentDateAndTime}}
+          ]
+        }
+      } else if (eventTime === "finished") {
+        dbQuery = {
+          eventType: eventType,
+          $and: [
+            {startDate: {$lt: currentDateAndTime}},
+            {endDate: {$lt: currentDateAndTime}}
+          ]
+        }
+      } else {
+        res.status(500).json({
+          error: true,
+          message: "Time value tidak valid!"
+        })
+      }
+    }
+
+    Event.find(dbQuery)
+        .then(r => {
+          if (position === "admin") {
+            return res.status(200).json({error: false, data: {events: r}})
+          }
+
+          res.status(200).json({
+            error: false,
+            data: {events: r.filter(item => item.eventDivision.toString() === division.toString())}
+          })
+        })
+        .catch(e => {
+          console.log(e)
+          res.status(500).json({
+            error: true,
+            message: "Gagal mencari data Event, silahkan coba beberapa saat lagi."
           })
         })
   }
--- app/event/router.js
+++ app/event/router.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {addEventData, editEventData} = require("./controller");
-const {requireAdmin} = require("../../middleware/authentication");
+const {addEventData, editEventData, findEventData} = require("./controller");
+const {requireAdmin, requireAdminOrMember} = require("../../middleware/authentication");
 const router = express.Router()
 
 router.post('/', requireAdmin, addEventData)
 router.put('/', requireAdmin, editEventData)
+router.get('/', requireAdminOrMember, findEventData)
 
 module.exports = router
