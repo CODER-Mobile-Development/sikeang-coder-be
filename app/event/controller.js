9b695292505cd17bef87d54938d22d724c0b31e2
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-11-22 10:13:12 +0700

    improvements
    - Added getPreSignedKeyUpload for obtaining upload url
    - Added eventLocation field in editEventData

--- app/event/controller.js
+++ app/event/controller.js
@@ -1,5 +1,10 @@
 const Event = require('../../model/Event')
+const Division = require('../../model/Division')
 const moment = require("moment");
+const {v4: uuidv4} = require('uuid');
+const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3");
+const {getSignedUrl} = require("@aws-sdk/s3-request-presigner");
+const {R2_ENDPOINT, R2_ACCESS_KEY, R2_ACCESS_SECRET, R2_BUCKET, R2_DOMAIN} = require("../../config/env");
 
 module.exports = {
   addEventData: (req, res) => {
@@ -18,24 +23,36 @@ module.exports = {
         })
   },
   editEventData: (req, res) => {
-    const {eventName, startDate, endDate, description, photoUrl, eventType, eventDivision} = req.body;
+    const {eventName, startDate, endDate, description, photoUrl, eventType, eventLocation, eventDivision} = req.body;
     const {id} = req.query
 
-    const updatedData = {}
+    Division.findOne({divisionName: 'Global'})
+        .then(divisionGlobal => {
+          const {_id: divisionGlobalId} = divisionGlobal
+          const updatedData = {}
 
-    if (eventName) updatedData.eventName = eventName
-    if (startDate) updatedData.startDate = startDate
-    if (endDate) updatedData.endDate = endDate
-    if (description) updatedData.description = description
-    if (photoUrl) updatedData.photoUrl = photoUrl
-    if (eventType) updatedData.eventType = eventType
-    if (eventDivision) updatedData.eventDivision = eventDivision
+          if (eventName) updatedData.eventName = eventName
+          if (startDate) updatedData.startDate = startDate
+          if (endDate) updatedData.endDate = endDate
+          if (description) updatedData.description = description
+          if (photoUrl) updatedData.photoUrl = photoUrl
+          if (eventType) updatedData.eventType = eventType
+          if (eventDivision) updatedData.eventDivision = eventType === 'global' ? divisionGlobalId : eventDivision
+          if (eventLocation) updatedData.eventLocation = eventLocation
 
-    Event.findByIdAndUpdate(id, updatedData, {new: true})
-        .then(r => {
-          res.status(200).json({error: false, data: r})
+          Event.findByIdAndUpdate(id, updatedData, {new: true})
+              .then(r => {
+                res.status(200).json({error: false, data: r})
+              })
+              .catch(e => {
+                console.log(e)
+                res.status(500).json({
+                  error: true,
+                  message: "Gagal mengubah data Event, silahkan coba beberapa saat lagi."
+                })
+              })
         })
-        .catch(e => {
+        .catch((e) => {
           console.log(e)
           res.status(500).json({
             error: true,
@@ -45,7 +62,7 @@ module.exports = {
   },
   findEventData: async (req, res) => {
     const {query, id, eventTime, eventType} = req.query
-    const {division: userDivision, position} = req.userData
+    const {division: userDivision, position, _id} = req.userData
 
     let dbQuery = {}
 
@@ -90,27 +107,51 @@ module.exports = {
             return res.status(200).json({error: false, data: {events: r}})
           }
 
-          if (eventType === 'division') {
-            console.log(r.filter(item =>
-                item.eventDivision.toString() === userDivision._id.toString()
-            ))
-            return res.status(200).json({
-              error: false,
-              data: {
-                events: r.filter(item =>
-                    item.eventDivision.toString() === userDivision._id.toString()
-                )
-              }
+          if (query === 'eventStatus') {
+            if (eventType === 'division') {
+              return res.status(200).json({
+                error: false,
+                data: {
+                  events: r.filter(item =>
+                      item.eventDivision.toString() === userDivision._id.toString()
+                  )
+                }
+              })
+            }
+
+            if (eventType === 'global') {
+              return res.status(200).json({
+                error: false,
+                data: {
+                  events: r.filter(item =>
+                      item.eventType === 'global'
+                  )
+                }
+              })
+            }
+
+            return res.status(400).json({
+              error: true,
+              message: 'Request not valid! missing eventType query parameter.'
             })
           }
 
-          if (eventType === 'global') {
+          if (query === 'id') {
+            if (r.length > 0 && r[0].eventType === 'division') {
+              return res.status(200).json({
+                error: false,
+                data: {
+                  events: r.filter(item =>
+                      item.eventDivision.toString() === userDivision._id.toString()
+                  )
+                }
+              })
+            }
+
             return res.status(200).json({
               error: false,
               data: {
-                events: r.filter(item =>
-                    item.eventType === 'global'
-                )
+                events: r
               }
             })
           }
@@ -125,6 +166,43 @@ module.exports = {
           res.status(500).json({
             error: true,
             message: "Gagal mencari data Event, silahkan coba beberapa saat lagi."
+          })
+        })
+  },
+  getPreSignedKeyUpload: (req, res) => {
+    const {fileFormat} = req.query
+    const key = `sikeang-events/${uuidv4()}.${fileFormat}`
+
+    const S3 = new S3Client({
+      endpoint: R2_ENDPOINT,
+      credentials: {
+        accessKeyId: R2_ACCESS_KEY,
+        secretAccessKey: R2_ACCESS_SECRET
+      },
+      region: "auto",
+    });
+
+    getSignedUrl(S3,
+        new PutObjectCommand({
+          Bucket: R2_BUCKET,
+          Key: key,
+        }),
+        {
+          expiresIn: 3600
+        }
+    )
+        .then((r) => res.status(200).json({
+          error: false,
+          data: {
+            url: r,
+            photoURI: `${R2_DOMAIN}/${key}`
+          }
+        }))
+        .catch((e) => {
+          console.log(e)
+          res.status(500).json({
+            error: true,
+            message: "Gagal meminta upload file, silahkan coba beberapa saat lagi."
           })
         })
   }
--- app/event/router.js
+++ app/event/router.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {addEventData, editEventData, findEventData} = require("./controller");
+const {addEventData, editEventData, findEventData, getPreSignedKeyUpload} = require("./controller");
 const {requireAdmin, requireAdminOrMember} = require("../../middleware/authentication");
 const router = express.Router()
 
 router.post('/', requireAdmin, addEventData)
 router.put('/', requireAdmin, editEventData)
 router.get('/', requireAdminOrMember, findEventData)
+router.get('/upload-request', requireAdmin, getPreSignedKeyUpload)
 
 module.exports = router
