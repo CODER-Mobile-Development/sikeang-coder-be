b3fc82a58e989c024ed75d70039fb3e3cc613893
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-12-04 07:30:32 +0700

    added updateDivisionData endpoint

--- app/division/controller.js
+++ app/division/controller.js
@@ -1,4 +1,5 @@
 const Division = require('../../model/Division')
+const User = require('../../model/User')
 
 module.exports = {
   addDivisionData: (req, res) => {
@@ -25,5 +26,22 @@ module.exports = {
         .catch(e =>
             res.status(500).json({error: true, message: `Error: ${e.toString()}`})
         )
+  },
+  updateDivisionData: (req, res) => {
+    const {divisionId} = req.params
+    const {divisionName} = req.body
+
+    Division.findByIdAndUpdate(divisionId, {divisionName})
+        .then(() => {
+          res.status(200).json({
+            error: false,
+            data: null
+          })
+        })
+        .catch(e => res.status(500).json({
+          error: true,
+          message: e.toString()
+        }))
+  },
   }
 }
--- app/division/router.js
+++ app/division/router.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const {addDivisionData, getAllDivisionData} = require("./controller");
+const {
+  addDivisionData,
+  getAllDivisionData,
+  updateDivisionData,
+} = require("./controller");
 const {requireAdmin} = require("../../middleware/authentication");
 const router = express.Router()
 
 router.post('/', requireAdmin, addDivisionData)
 router.get('/', requireAdmin, getAllDivisionData)
+router.put('/:divisionId', requireAdmin, updateDivisionData)
 
 module.exports = router
