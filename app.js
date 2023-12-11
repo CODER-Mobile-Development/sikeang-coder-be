9bc99500d448aeded4d12d6044a7f938f2df03de
Irvan Surya Nugraha <irvansurya1@gmail.com>
2023-12-11 19:34:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const dashboardRouter = require('./app/dashboard/router');
 const authRouter = require('./app/auth/router');
@@ -9,6 +10,7 @@ const eventRouter = require('./app/event/router');
 const divisionRouter = require('./app/division/router');
 const pointTransactionRouter = require('./app/transaction/router');
 const userRouter = require('./app/user/router');
+const {MONGO_URI} = require("./config/env");
 
 const app = express();
 
@@ -24,5 +26,11 @@ app.use('/event', eventRouter);
 app.use('/division', divisionRouter);
 app.use('/point-transaction', pointTransactionRouter);
 app.use('/user', userRouter);
+
+mongoose?.connect(MONGO_URI)
+    .then(() => console.log('Connected to database!'))
+    .catch((e) => {
+      throw new Error(e);
+    });
 
 module.exports = app;
